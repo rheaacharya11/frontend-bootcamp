cc405530838962a14ad932f51d9abce3f58a0160
Sander Kromwijk <sanderk@palantir.com>
2018-08-08 15:34:43 -0400

    Clarify functional programming tests

--- 05-AboutFunctionalProgramming.js
+++ 05-AboutFunctionalProgramming.js
@@ -8,7 +8,7 @@ describe("About Functional Programming", function () {
     // FILL_ME_IN
   }
 
-  it("should use filter to return array items that meet a criteria", function () {
+  it("should use the 'filter' function to return array items that meet a criteria", function () {
     expect(onlyOddNumbers([1,2,3])).toEqual(referenceOnlyOddNumbers([1,2,3]));
     expect(onlyOddNumbers([1,2,4])).toEqual(referenceOnlyOddNumbers([1,2,4]));
     expect(onlyOddNumbers([1,2,4,3])).toEqual(referenceOnlyOddNumbers([1,2,4,3]));
@@ -19,7 +19,7 @@ describe("About Functional Programming", function () {
     // FILL_ME_IN
   }
 
-  it("should use 'map' to transform each element", function () {
+  it("should use the 'map' function to transform each element", function () {
     expect(incrementAllBy([1,2,3], 1)).toEqual(referenceIncrementAllBy([1,2,3], 1));
     expect(incrementAllBy([1,2,4], 3)).toEqual(referenceIncrementAllBy([1,2,4], 3));
   });
@@ -29,7 +29,7 @@ describe("About Functional Programming", function () {
     // FILL_ME_IN
   }
 
-  it("should use 'reduce' to update the same result on each iteration", function () {
+  it("should use the 'reduce' function to update the same result on each iteration", function () {
     expect(productOf([1,2,3])).toEqual(referenceProductOf([1,2,3]));
     expect(productOf([2,3,5,7])).toEqual(referenceProductOf([2,3,5,7]));
   });
@@ -45,7 +45,7 @@ describe("About Functional Programming", function () {
     return capture;
   }
 
-  it("should use 'forEach' for simple iteration", function () {
+  it("should use the 'forEach' function for simple iteration", function () {
     expect(captureForEach([1,2,3], callOnEveryElement)).toEqual(captureForEach([1,2,3], referenceCallOnEveryElement));
     expect(captureForEach([2,3,5,7], callOnEveryElement)).toEqual(captureForEach([2,3,5,7], referenceCallOnEveryElement));
   });
@@ -55,7 +55,7 @@ describe("About Functional Programming", function () {
     // FILL_ME_IN
   }
 
-  it("should use 'every' to test whether all items pass condition", function () {
+  it("should use the 'every' function to test whether all items pass condition", function () {
     expect(areAllEven([1,2,3])).toEqual(referenceAreAllEven([1,2,3]));
     expect(areAllEven([2,4,6])).toEqual(referenceAreAllEven([2,4,6]));
   });
@@ -65,7 +65,7 @@ describe("About Functional Programming", function () {
     // FILL_ME_IN
   }
 
-  it("should use 'some' to test if any items passes condition" , function () {
+  it("should use the 'some' function to test if any items passes condition" , function () {
     expect(areAnyEven([1,2,3])).toEqual(referenceAreAnyEven([1,2,3]));
     expect(areAnyEven([1,5,3])).toEqual(referenceAreAnyEven([1,5,3]));
   });
@@ -75,7 +75,7 @@ describe("About Functional Programming", function () {
     // FILL_ME_IN
   }
 
-  it("should use filter, map, reduce to compute a complex operation", function() {
+  it("should use the 'filter', 'map', and 'reduce' functions to compute a complex operation", function() {
     expect(sumOfSquaresOfEvens([1,2,3])).toEqual(referenceSumOfSquaresOfEvens([1,2,3]));
     expect(sumOfSquaresOfEvens([1,2,3,4,5,6])).toEqual(referenceSumOfSquaresOfEvens([1,2,3,4,5,6]));
     expect(sumOfSquaresOfEvens([1,3,5])).toEqual(referenceSumOfSquaresOfEvens([1,3,5]));
